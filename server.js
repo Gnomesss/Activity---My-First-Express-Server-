823c33f99dd259550d430b25609d28653063bf27
Aljon <ynot.aljon@gmail.com>
2025-10-05 09:17:10 +0800

    Activity: “My First Express Server”

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
